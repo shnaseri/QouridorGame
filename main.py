5b573ebdaafb80898d3513d7cc95d4f8f7706ef2
shnaseri <hnaseri.iust@gmail.com>
2021-05-17 18:58:54 +0430

    block is inited

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from Person import Person
 from Wall import Wall
 from Block import Block
+blocks = []
+def initmatrix():
+    global blocks
+    for i in range(10):
+        for j in range(10):
+            if i == 0 and j == 5:
+                blocks.append(Block(i,j,'G'))
+            elif i == 9 and j == 5:
+                blocks.append(Block(i,j,'R'))
+            else:
+                blocks.append(Block(i,j))
+
+initmatrix()
