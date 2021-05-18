b19df74fe65b263872c9a30e56fabf1b2c57fbf7
Mehdi Mostafavi <seyedmehdimostafavi79@gmail.com>
2021-05-18 18:41:21 +0430

    Fixed wall bug

--- Wall.py
+++ Wall.py
@@ -9,10 +9,10 @@ class Wall:
     @staticmethod
     def hasWall(wall):
         if wall.orientation == 'H':
-            if wall.startX in [i.x for i in Wall.walls] or wall.startX + 1 in [i.x for i in Wall.walls]:
+            if wall.startX in [i.startX for i in Wall.walls] or wall.startX + 1 in [i.startX for i in Wall.walls]:
                 return True
         else:
-            if wall.startY in [i.y for i in Wall.walls] or wall.startY + 1 in [i.y for i in Wall.walls]:
+            if wall.startY in [i.startY for i in Wall.walls] or wall.startY + 1 in [i.startY for i in Wall.walls]:
                 return True
         return False
 
@@ -21,9 +21,9 @@ class Wall:
             return False
         elif person.wallsRemain < 1:
             return False
-        elif wall.orientation == 'H' and wall.startX > (n - 1):
+        elif wall.orientation == 'H' and wall.startX > (7):
             return False
-        elif wall.orientation == 'V' and wall.startY > (n - 1):
+        elif wall.orientation == 'V' and wall.startY > (7):
             return False
         elif wall.startY < 0 or wall.startX < 0:
             return False
